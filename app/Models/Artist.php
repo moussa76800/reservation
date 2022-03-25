2ef78658b24a5b5bf6a9045d74da71f9f1882571
moussa76800 <80781595+moussa76800@users.noreply.github.com>
2022-03-25 10:51:50 +0100

    Modèle Artist +migration +seeders

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Artist extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['firstname', 'lastname'];
+
+   /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'artists';
+
+   /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
+
+
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        $this->call([
+            ArtistSeeder::class,
+        ]);
+
         // \App\Models\User::factory(10)->create();
     }
 }
